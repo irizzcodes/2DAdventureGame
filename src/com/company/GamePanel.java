11ddabadcce719ac69d3fabfca3b12c670133898
irizzcodes <114680318+irizzcodes@users.noreply.github.com>
2022-12-02 19:47:45 +0800

    Part 2 - Add Key Inputs, Add FPS

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -16,12 +16,24 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxscreenRow; // 576 pixels
 
+    // Frame per second
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+
     Thread gameThread; // to repeat process again and again every 1 second
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -29,8 +41,99 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
 
+    /* !!!!!!!!!!!!!!!!!!!!!!!!! THREAD SLEEP METHOD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     @Override
     public void run() {
+        double drawInterval = 1000000000/FPS; // precise calculation, dividing nanoseconds by FPS, 1 billion ns = 1 sec
+        double nextDrawTime = System.nanoTime() + drawInterval;
+        // System.nanoTime() = return current value of JVM High-R time source in nanoseconds
+
+        while (gameThread != null){
+            update();
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000; // because sleep only accepts milliseconds
+
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime); // to do nothing while waiting for the next loop
+
+                nextDrawTime += drawInterval;
+            }
+
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+     */
+
+    public void run(){
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        // for variables to display FPS
+        long timer = 0;
+        int drawCount = 0;
+
+
+        while (gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                // Update information for character position
+                update();
+                // Update information for the screen
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            // to display FPS
+            if (timer >= 1000000000){
+                System.out.println("FPS: "+ drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+        }
+    }
+
+
+
+    public void update(){
+        if (keyH.upPressed == true){
+            playerY -= playerSpeed; // playerY = playerY - playerSpeed
+        }
+        else if (keyH.downPressed == true){
+            playerY += playerSpeed;
+        }
+        else if (keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        }
+        else if (keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
+
+    }
+
+    // Note!! Graphics class has many functions to draw objects on screen
+    public void paintComponent(Graphics g){
+        super.paintComponent(g); // super for the parent class of this class
+
+        // Graphics2D extends the Graphic Class  to provide more control coordinate, color, text layout
+        Graphics2D g2 = (Graphics2D)g; // we change graphic g to graphic 2d class
+        g2.setColor(Color.white);
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        g2.dispose();
 
     }
 }
